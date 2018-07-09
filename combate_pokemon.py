15f8b6f921163cfe001f542614b13d917c2ab824
Nicolás Hervith <niko_hervith@hotmail.com>
2018-07-09 16:27:26 -0300

    Combate pokemon

--- combate_pokemon.py
+++ combate_pokemon.py
@@ -1,10 +1,19 @@
 
 pokemon_elegido = input("¿Contra quién querés luchar? (Squirtle, Charmander, Bulbasaur)")
+#opciones por si la respuesta no es squirtle, charmander o bulbasaur
+if pokemon_elegido == "charmander":
+    pokemon_elegido = True
+elif pokemon_elegido == "bulbasaur":
+    pokemon_elegido = True
+elif pokemon_elegido == "squirtle":
+    pokemon_elegido = True
+else:
+    print("no se que dijiste, pero perdiste")
 
 vida_pikachu = 100
-vkda_enemigo = 0
+vida_enemigo = 0
 ataque_pokemon = 0
-
+#elif para ahorrar espacios
 if pokemon_elegido == "squirtle":
     vida_enemigo = 90
     nombre_pokemon = "squirtle"
@@ -27,7 +36,7 @@ while vida_pikachu > 0 and vida_enemigo > 0:
       vida_enemigo -= 10
     if ataque_elegido == "voltio":
       vida_enemigo -= 12
-
+    #mostramos el resultado del ataque
     print("la vida de {} es {}".format(nombre_pokemon, vida_enemigo))
 
     print("{} te hace un ataque de {} de daño".format(nombre_pokemon, ataque_pokemon))
