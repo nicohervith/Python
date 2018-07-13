ff2c538e3649246c913991d0b8f8edfec32550b7
Nicolás Hervith <niko_hervith@hotmail.com>
2018-07-12 21:43:13 -0300

    Grados celsius a farenheit y viceversa

--- Grados_celsius_farenheit.py
+++ Grados_celsius_farenheit.py
@@ -6,6 +6,13 @@ if user == "si":
     grados_celsius = int(input("introduce la cantidad de grados Celsius que deseas transformar"))
     grados_farenheit = (grados_celsius * 1.8) + 32
     print("{} grados celsius son {} grados farenheit".format(grados_celsius, grados_farenheit))
-else:
-    print("ok chao :v")
+
+elif user =="no":
+    user2 = input("¿Querés transformar grados Farenheit a Celsius? (si/no)")
+    if user2 == "si":
+        grados_f = int(input("introduce la cantidad de grados Farenheit que deseas transformar"))
+        grados_c = (grados_f / 1.8 ) - 32
+        print("{} grados farenheit son {} grados celsius".format(grados_f, grados_c))
+
+print("chao")
 
