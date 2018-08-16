724e3a3bea669e3d1c7d3d57402131497d22b93c
Nicolás <niko_hervith@hotmail.com>
2018-08-15 23:38:13 -0300

    Asignando funciones

---
+++ Test_Funciones.py
@@ -0,0 +1,16 @@
+
+def input_con_confirmacion(pregunta):
+    confirmacion = False
+    dato_usuario = ""
+    while not confirmacion:
+        dato_usuario = input(pregunta)
+        seguro = input("Dijiste {}, ¿Estás seguro?  [ si / no ]").format(dato_usuario)
+        if seguro == "si":
+            confirmacion = True
+    return dato_usuario
+
+nombre = input_con_confirmacion("¿Cómo te llamás?")
+print("Confirmaste que te llamás {}".format(nombre))
+
+numero = input_con_confirmacion("Dime un número")
+print("Confirmaste que el numero es {}".format(numero))
