685714e2c1b4692b44839fd4aef16dcd2dc9923f
Nicolás <niko_hervith@hotmail.com>
2020-09-30 11:50:26 -0300

    Area de un triángulo

---
+++ Pruebas.py
@@ -0,0 +1,11 @@
+
+#Hallar area de un triángulo
+
+#Formula : (base x altura) / 2
+
+B = float(input("Ingrese la base: "))
+H = float(input("ingrese la altura: "))
+area = (B*H)/2
+
+print("El area del triángulo es: ",area)
+
