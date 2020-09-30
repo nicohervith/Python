71b0bfa300b380028d9bc1fa759e72342a46af20
Nicolás <niko_hervith@hotmail.com>
2020-09-30 12:17:40 -0300

    Promediar notas

---
+++ promediar_notas.py
@@ -0,0 +1,25 @@
+
+#Promedio de notas
+
+'''
+N1 = primera nota
+N2 = Segunda nota
+N3 = Tercera nota
+
+'''
+
+N1 = float(input('Ingrese la primer nota: '))
+N2 = float(input('Ingrese la segunda nota: '))
+N3 = float(input('Ingrese la tercer nota: '))
+
+Promedio = (N1 + N2 + N3)/3
+
+print("El promedio de las notas es: "+str(Promedio)+' puntos.')
+
+'''
+Cuando mi frase termina ej: "el promedio es" puedo poner ,Promedio
+
+Al agregar 'Puntos.' debo poner +str(Promedio)+
+
+
+'''
