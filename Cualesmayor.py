dd3b0269314df3fe4038a49cf4376d09e7f499d7
Nicolás <niko_hervith@hotmail.com>
2020-10-01 12:58:01 -0300

    if/else, repaso

---
+++ Cualesmayor.py
@@ -0,0 +1,8 @@
+
+a = int(input(" Ingrese la variable a: "))
+b = int(input("Ingrese lsa variable b: "))
+
+if a>b:
+    print("La variable A es mayor que B")
+else:
+    print("La variable B es mayor que A")
