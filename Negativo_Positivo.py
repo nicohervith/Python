7ef66c1be5aa5596b7c17c2c8221873a0d621491
Nicolás <niko_hervith@hotmail.com>
2020-10-01 12:51:21 -0300

    if/else, repaso

---
+++ Negativo_Positivo.py
@@ -0,0 +1,8 @@
+# Utilización de If/Else. Ejemplo con saber si un número es positivo o negativo
+
+A = int(input("Ingrese un número: "))
+if  A >= 0:
+    print("Positivo")
+
+else:
+    print("Negativo")
