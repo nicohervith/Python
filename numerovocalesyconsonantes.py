46be5041e2b309e4e4615755e3f8779b70e701e9
Nicolás <niko_hervith@hotmail.com>
2020-10-06 12:47:49 -0300

    vocales y consonantes mejorado, uso de for

--- numerovocalesyconsonantes.py
+++ numerovocalesyconsonantes.py
@@ -1,7 +1,7 @@
 
 frase_usuario = input("Dime una frase: ")
 
-vocales = ("a", "e", "i", "o", "u")
+vocales = ("a", "e", "i", "o", "u", "á", "é", "í", "ó", "ú")
 
 n_vocales = 0
 n_consonantes = 0
@@ -14,3 +14,8 @@ for letra in frase_usuario:
 
 print("El número de vocales es {}".format(n_vocales))
 print("El número de consonantes es {}".format(n_consonantes))
+
+'''
+El número de consonantes no está bien ya que cuenta los espacios, comas, puntos, etc..
+
+'''
