18bf15d93eb4307d794cc32d7521e95522d03052
Nicolás <niko_hervith@hotmail.com>
2020-10-06 12:31:49 -0300

    While

---
+++ ascendenteydescendente.py
@@ -0,0 +1,19 @@
+# Ingresar los N primeros números de forma ascendente
+
+n = int(input("Ingrese un número: "))
+i = 1
+
+while i <= n:
+    print(i)
+    i = i +1
+
+'''
+Descendente
+ 
+ n = int(input("ingrese un número: ))
+ i = n
+ while i >= 1:
+    print(i)
+    i = i-1
+
+'''
