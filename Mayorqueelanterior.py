08c49a60d8005be5ff8a60107c318ff7d8dd22d2
Nicolás <niko_hervith@hotmail.com>
2020-10-07 11:02:51 -0300

    range

--- Mayorqueelanterior.py
+++ Mayorqueelanterior.py
@@ -1,4 +1,5 @@
 
+# PARA QUE ME TOME EN CUENTA LA VARIABLE PONGO F EN EL PRINCIPIO
 
 valor = int(input("Cuántos valores vas a introducir? \n"))
 
@@ -8,7 +9,7 @@ else:
     primero = int(input("Escriba un número: "))
     for i in range (valor - 1):
         numero = int(input((f"escriba un número más grande que {primero} : ")))
-
+        # PARA QUE ME TOME EN CUENTA LA VARIABLE PONGO F EN EL PRINCIPIO
         if numero <= primero:
             print(f"{numero} no es mayor que {primero}")
         primero = numero
