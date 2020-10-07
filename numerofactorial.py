856cde1f60cb2d590b53a1e3e79c8af8e06d6c46
Nicolás <niko_hervith@hotmail.com>
2020-10-07 11:56:53 -0300

    for y range

---
+++ numerofactorial.py
@@ -0,0 +1,15 @@
+# NUMERO FACTORIAL
+# EJEMPLO:
+# 2! = 1 * 2 = 2
+# 3! = 1 * 2 * 3 = 6
+# 8! = 1 * 2 * 3 * 4 * 5 * 6 * 7 * 8 = 40320
+
+n = int(input("Ingrese un número: "))
+
+if n < 0:
+    print("Ingrese un número positivo")
+else:
+    factorial = 1
+    for i in range(1, n + 1):
+        factorial = factorial * i
+    print(f"El factorial de {n} es {factorial}")
