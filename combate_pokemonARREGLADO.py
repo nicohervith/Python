2309f100e997aff1e48752878318c90a516667c2
Nicolás <niko_hervith@hotmail.com>
2020-10-07 12:14:59 -0300

    Combate pokemon arreglado

--- combate_pokemonARREGLADO.py
+++ combate_pokemonARREGLADO.py
@@ -3,17 +3,21 @@ pokemon_elegido = input("¿Contra quién querés luchar? (Squirtle, Charmander, 
 #opciones por si la respuesta no es squirtle, charmander o bulbasaur
 if pokemon_elegido == "charmander":
     pokemon_elegido = True
+    nombre_pokemon = "charmander"
 elif pokemon_elegido == "bulbasaur":
     pokemon_elegido = True
+    nombre_pokemon = "bulbasaur"
 elif pokemon_elegido == "squirtle":
     pokemon_elegido = True
+    nombre_pokemon = "squirtle"
 else:
     print("no se que dijiste, pero perdiste")
 
 vida_pikachu = 100
-vida_enemigo = 0
-ataque_pokemon = 0
+vida_enemigo = 100
+ataque_pokemon = 10
 #elif para ahorrar espacios
+
 if pokemon_elegido == "squirtle":
     vida_enemigo = 90
     nombre_pokemon = "squirtle"
@@ -30,14 +34,26 @@ elif pokemon_elegido == "bulbasaur":
     ataque_pokemon = 10
 
 while vida_pikachu > 0 and vida_enemigo > 0:
-    ataque_elegido = input("¿Que ataque querés usar? ( Chispazo / Voltio )")
-
+    ataque_elegido = input("¿Que ataque querés usar? ( Chispazo / Voltio / Combinados )")
+    chispazo = 1
+    voltio = 2
+    combinados = 3
     if ataque_elegido == "chispazo":
       vida_enemigo -= 10
     if ataque_elegido == "voltio":
       vida_enemigo -= 12
+    if ataque_elegido == "combinados":
+      vida_enemigo -=23
+    if ataque_elegido == "1":
+      vida_enemigo -= 10
+    if ataque_elegido == "2":
+      vida_enemigo -=12
+    if ataque_elegido == "3":
+      vida_enemigo -= 23
+
+
     #mostramos el resultado del ataque
-    print("la vida de {} es {}".format(nombre_pokemon, vida_enemigo))
+    print(f"la vida de {nombre_pokemon} es {vida_enemigo}")
 
     print("{} te hace un ataque de {} de daño".format(nombre_pokemon, ataque_pokemon))
     vida_pikachu -= ataque_pokemon
