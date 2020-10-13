3c5e2becdbe036815d0729bb2a896633a6e60919
Nicolás <niko_hervith@hotmail.com>
2020-10-13 11:49:48 -0300

    diccionario, conjuntos

---
+++ conjuntosset.py
@@ -0,0 +1,8 @@
+
+alumo = set()
+
+alumno = {"Nicolas", "juan", "sergio"}
+
+
+#ESTO ME PERMITE BUSCAR UN DATO EN LA CADENA, SI SE ENCUENTRA LA RESPUESTA ES TRUE, SINO FALSE
+print('Nicolas' in alumno)
---
+++ diccionario.py
@@ -0,0 +1,5 @@
+#diccionario
+edades = {'victor':30,'maria':24,'franco':29}
+
+for i in edades:
+    print(i,edades[i])
