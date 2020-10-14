33955a71b4bc240627334a8d9a8d19119cbf7b9e
Nicolás <niko_hervith@hotmail.com>
2020-10-14 11:10:13 -0300

    del, in

---
+++ usandoDel.py
@@ -0,0 +1,11 @@
+#del
+
+curso = ('python')
+
+lista_nueva = list(curso)
+
+del lista_nueva[2]
+
+print(lista_nueva)
+
+#ME ELIMINA LA POSICIÓN QUE INDIQUE DE LA LISTA, EN EL CASO DEL 2 ME ELIMINA LA 'T'
---
+++ usandoIn.py
@@ -0,0 +1,9 @@
+# IN
+
+curso = 'python'
+
+lista_nueva = list('python')
+print(lista_nueva)
+
+if 'y' in lista_nueva:
+    print('"y" es parte de la lista')
