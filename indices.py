7f5e8d39806182cc0d745f1608b68975aa44f420
Nicolás <niko_hervith@hotmail.com>
2020-10-14 10:57:31 -0300

    INDICES

---
+++ indices.py
@@ -0,0 +1,23 @@
+
+# EJEMPLOS INDICES
+
+curso = 'python'
+
+print(curso[2])
+
+al ejecutar me da una 't' porque 'p' es 0, 'y' es 1 y 't' es 2
+
+
+
+curso = 'python'
+
+print(curso[3:])
+on #PORQUE AL PONER 3: ME DA TODO LO QUE CONTENGA A LA DERECHA
+
+curso = 'python'
+print(curso[1:4])
+yth #LE INDICO QUE TIENE QUE TIENE QUE IMPRIMIR, EL SEGUNDO NUMERO SIEMPRE ES -1
+
+curso = 'python'
+print(curso.index[t])
+2 # ME DA UN 2 PORQUE BUSCO LA POSICION DE LA LETRA
