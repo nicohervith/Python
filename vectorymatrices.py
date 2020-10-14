f8bef9fae56618056fc84e15bdb08b10fc275d77
Nicolás <niko_hervith@hotmail.com>
2020-10-14 12:14:23 -0300

    VECTORES

---
+++ vectorymatrices.py
@@ -0,0 +1,12 @@
+# VECTORES Y MATRICES
+
+import numpy as np
+
+vector = np.array([1,2,3,4])
+matriza = np.array([[1,2,3],[4,5,6],[7,8,9]])
+matrizb = np.array([[1,2,3],[4,5,6],[7,8,9]])
+
+print('vector ',vector)
+print('matriz ',matriza + matrizb)
+
+#LOS NUMEROS DE LA MATRIZ SE SUMAN EN ORDEN
